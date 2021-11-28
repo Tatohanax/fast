0922b446287875bba3ffe3e177192c0879a698b1
tatohanax <antonchugunov@MacBook-Pro-Anton.local>
2021-11-28 18:57:15 +0300

    first try

--- app/src/routers.py
+++ app/src/routers.py
@@ -27,17 +27,12 @@ import re
 
 router = APIRouter()
 
-#@router.post("/", response_description="Add new user" )
-#async def create_user(user: UserModel):
-#    await user.save()
-#    return new_user
-
-@router.post("/", response_description="Add new", response_model=UserModel)
-async def create_user(user: UserModel):
-    user = jsonable_encoder(user)
-    new_user = await db["users"].insert_one(user)
-    created_user = await db["users"].find_one({"_id": new_user.inserted_id})
-    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_user)
+@router.post("/", response_description="Add new", response_model=UserModel) 
+async def create_user(user: UserModel): 
+    new_user = await db["users"].insert_one(user.dict())
+    created_user = await db["users"].find_one({"_id": new_user.inserted_id}) 
+    created_user.pop('_id')
+    return created_user
 
 
 @router.get("/list", response_description="List all users", response_model=List[ShowUserModel])
